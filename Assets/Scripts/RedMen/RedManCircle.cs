69a8e161e577eea325822281c4164912eafdaae8
Santi4go32 <quierocarne40@gmail.com>
2024-04-02 19:58:27 -0500

    add SpotLight to the redmanCircle

--- Assets/Scripts/RedMen/RedManCircle.cs
+++ Assets/Scripts/RedMen/RedManCircle.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class RedManCircle : MonoBehaviour
 {
     public Transform RedManCircleTransform;
+    public Light SpotLight;
     private float speed = 1.0f; 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        if(SpotLight != null){
+            SpotLight.transform.position = RedManCircleTransform.position + Vector3.up * 2;
+            SpotLight.color = Color.red;
+        }
     }
 
     // Update is called once per frame
